b991aae97477882a9a320560064a97924e4a02d8
yeanajun <qkqlalsk@naver.com>
2021-05-23 03:02:14 +0900

    have to check!!!

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,31 @@
+import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        int len = numbers.length;
+        String[] str = new String[len];
 
+        for (int i = 0; i < len; i++) {
+            str[i] = Integer.toString(numbers[i]);
+        }
 
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
 
+            }
+        });
 
+        if(str[0].equals("0"))
+            return "0";
 
+        for(String s: str)
+            answer += s;
 
 
         return answer;
     }
+
 }
